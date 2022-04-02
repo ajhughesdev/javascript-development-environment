6f320fad9f0f9b06ea9f6d0088d5fe22bb377d5e
Andrew J Hughes <andrew@ajhughes.dev>
2022-04-01 22:46:33 -0400

    set up bundling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./index.css";
-
 import { getUsers, deleteUser } from "./api/userApi";
 
 // Populate table of users via API call.
@@ -13,7 +12,7 @@ getUsers().then((result) => {
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
       <td>${user.email}</td>
-    </tr>`;
+      </tr>`;
   });
 
   global.document.getElementById("users").innerHTML = usersBody;
@@ -21,7 +20,7 @@ getUsers().then((result) => {
   const deleteLinks = global.document.getElementsByClassName("deleteUser");
 
   // Must use array.from to create a real array from a DOM collection
-  // getElementsByClassName only returns an "array like" object
+  // getElementsByClassname only returns an "array like" object
   Array.from(deleteLinks, (link) => {
     link.onclick = function (event) {
       const element = event.target;
--- src/index.test.js
+++ src/index.test.js
@@ -9,8 +9,8 @@ describe("Our first test", () => {
 });
 
 describe("index.html", () => {
-  it("should say h1 that says Users", () => {
-    const index = fs.readFileSync("./src/index.html", "utf8");
+  it("should have h1 that says users", () => {
+    const index = fs.readFileSync("./src/index.html", "utf-8");
     const { JSDOM } = jsdom;
     const dom = new JSDOM(index);
     const h1 = dom.window.document.getElementsByTagName("h1")[0];
